06a9f00b4228f425376c58801b12dc7debaa082e
Erez Geva <ErezGeva2@gmail.com>
2024-08-04 19:41:48 +0200

    B3

--- Examples/test-suite/ruby/import_fragments_runme.rb
+++ Examples/test-suite/ruby/import_fragments_runme.rb
@@ -9,16 +9,15 @@
 
 require 'swig_assert'
 
-exception_file = nil
-
 begin
   require 'import_fragments'
-rescue LoadError => e
+#rescue LoadError => e
+rescue Exception => e
   # due to missing import_fragments_a
-  exception_file = e.respond_to?(:path) ? e.path : e.to_s.sub(/.* -- /, '')
+  puts "Error: #{e.message}"
+# exception_file = e.respond_to?(:path) ? e.path : e.to_s.sub(/.* -- /, '')
 end
 
-puts "exception_file=#{exception_file}"
 # swig_assert(exception_file == "import_fragments_a",
 #             msg: "Loading should have failed due to missing 'import_fragments_a'")
 
